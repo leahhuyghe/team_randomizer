7f5061d01cbdb1467d7eb69d74018144ec7a3b0d
robonoodles <heytnguyen@gmail.com>
2015-06-01 16:32:30 -0700

    committin

--- app.rb
+++ app.rb
@@ -8,8 +8,13 @@ end
 post "/randomizer" do
   @names = params[:student_names]
   @names.split(, )
+  @group_num = params[:group_number]
 
   names = []
+
+  if @group_num.to_i > @names.split(, ).length
+    @result = "number of teams too many"
+  end
   erb layout: :application
 
 end
