46e462657580b02e3862251f04d7fe19c528945b
Tien N <heytnguyen@gmail.com>
2015-06-03 14:33:06 -0700

    updated code; things are running now

--- tester.rb
+++ tester.rb
@@ -3,6 +3,9 @@ names = ["tien", "leah", "luke","josh","steve","karen","bran"]
 
 group_size = 9
 
+if group_size > names.length
+puts "fail"
+end
 
 arrays = Array.new(group_size) { [] }
 names.shuffle.each_with_index do |name, i|
